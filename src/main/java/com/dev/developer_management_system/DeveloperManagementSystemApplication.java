629ae98373f86b84cb419725e36fe555efdd5d0a
leeJH <ljh0200@outlook.kr>
2026-03-09 11:17:57 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/dev/developer_management_system/DeveloperManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dev.developer_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/developer_management_system/DeveloperManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.developer_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
